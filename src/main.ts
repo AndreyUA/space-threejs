937680e6de3f8b3322ad74322d8e851708be7f1c
AndreyUA <infernalsteel666@gmail.com>
2024-08-15 09:40:17 +0300

    basic implementation of particles

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,107 @@
-console.log("hello world");
+import "./style.css";
+
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GUI from "lil-gui";
+
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Debug
+const gui = new GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
+
+// Scene
+const scene = new THREE.Scene();
+
+// Textures
+const textureLoader = new THREE.TextureLoader();
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 20;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Resize listener
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Particles
+const particlesGeometry = new THREE.BufferGeometry(); // Custom geometry
+const count = 20_000;
+const arrayLength = count * 3;
+const positionsArray = new Float32Array(arrayLength);
+const colorsArray = new Float32Array(arrayLength);
+
+// Fill the geometry and color arrays with random values
+for (let i = 0; i < arrayLength; i++) {
+  positionsArray[i] = (Math.random() - 0.5) * 10;
+  colorsArray[i] = Math.random();
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positionsArray, 3)
+);
+particlesGeometry.setAttribute(
+  "color",
+  new THREE.BufferAttribute(colorsArray, 3)
+);
+
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.1,
+  sizeAttenuation: true,
+  vertexColors: true,
+});
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
+
+// ! Animation
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
